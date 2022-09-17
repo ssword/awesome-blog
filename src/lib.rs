a3d98ec49de032502d25bb2e594d50d5a314a9fd
ssword <ssword@gmail.com>
2022-09-17 15:59:59 +0800

    before handlers

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use actix_web::{dev::Server, middleware, web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
+use tera::Tera;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec![".html", ".sql"]);
+        tera
+    };
+}
+
+pub fn start_blog(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let srv = HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .route("/health", web::get().to(HttpResponse::Ok))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(srv)
+}
